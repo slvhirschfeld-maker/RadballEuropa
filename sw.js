c9499dfc49d52c62e645ef4674219915cda0c1d9
Gordon <hirschfeld@slv-halle.de>
2026-05-08 11:36:37 +0200

    Serviceworker url angepasst

--- sw.js
+++ sw.js
@@ -1,6 +1,10 @@
 const CACHE = "radball-v1";
-const FILES = ["/RadballEuropa/index.html", "/RadballEuropa/manifest.json", "/RadballEuropa/icons/icon-192.png", "/RadballEuropa/icons/icon-512.png"];
-
+const FILES = [
+  "/RadballEuropa/index.html",
+  "/RadballEuropa/manifest.json",
+  "/RadballEuropa/icons/icon-192.png",
+  "/RadballEuropa/icons/icon-512.png"
+];
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
 });
